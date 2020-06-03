48d264682b98aba199bd41d6f424ac3288a68c09
Andrew <andrewryanogle@gmail.com>
2020-06-03 13:09:33 -0700

    validating user info

--- CCBackend/data/model/userModel.js
+++ CCBackend/data/model/userModel.js
@@ -1,20 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const User = new Schema({
-    firstName : {type: String,
-    required: true},
-    lastName : {type: String,
-        required: true},
-    userName : {type: String,
-        unique: true,
-        required: true},
-    password : {
-        type: String,
-        required: true
-    },
-    email :{type: String,
-        required: true},
-},{timestamps:true})
+const validateEmail = function (email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
 
-module.exports = mongoose.model('User',User);
+const User = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    userName: { type: String, unique: true, required: true },
+    password: {
+      type: String,
+      required: "Password is Required",
+      validate: [
+        function (input) {
+          return input.length >= 6;
+        },
+        "Password should be longer",
+      ],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+  
+    validate: [validateEmail, "Please fill a valid email address"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ]}
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", User);
